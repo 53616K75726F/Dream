a4fdf45e9cef3fd97fde31b031f3818d3aa9f18e
4C6F72656D <4C6F72656D@post.cz>
2020-01-21 19:24:04 +0100

    Adding rotation algorythm

--- script.js
+++ script.js
@@ -1,59 +1,27 @@
 
-
-
 function draw() {
   var canvas = document.getElementById('tutorial');
   var c = canvas.getContext('2d');
+  var ctx = canvas.getContext('2d');
 
-
-var x1 = 30;
-var dx1 = 1;
-var radius1 = 30;
-
-var x2 = 30;
-var dx2 = 2;
-var radius2 = 30;
-
-var x3 = 30;
-var dx3 = 3;
-var radius3 = 30;
-
-var x4 = 30;
-var dx4 = 4;
-var radius4 = 30;
+var COUNT = 30;
 
 function animate1() {
-  requestAnimationFrame(animate1);
-
 
   c.clearRect(0, 0, innerWidth, innerHeight);
 
-  c.beginPath();
-  c.fillRect(x1, 20, 20,20);
-  c.fillRect(x2, 45, 20, 20);
-  c.fillRect(x3, 70, 20, 20);
-  c.fillRect(x4, 95, 20, 20);
+// Matrix transformation
+ctx.translate(160 + COUNT, 85 + COUNT);
+ctx.rotate(Math.PI/64);
+ctx.scale(1, 1);
+ctx.translate(-160 - COUNT, -85 - COUNT);
 
-  if(x1 > ((innerWidth/100)*10) || x1 - radius1 < 0){
-    dx1 = -dx1;
-  }
+// Rotated rectangle
+ctx.fillStyle = 'red';
+ctx.fillRect(90 + COUNT, 70 + COUNT, 140, 30);
 
-  if(x2 > ((innerWidth/100)*10) || x2 - radius2 < 0){
-    dx2 = -dx2;
-  }
 
-  if(x3 > ((innerWidth/100)*10) || x3 - radius3 < 0){
-    dx3 = -dx3;
-  }
-
-  if(x4 > ((innerWidth/100)*10) || x4 - radius4 < 0){
-    dx4 = -dx4;
-  }
-
-x1 += dx1;
-x2 += dx2;
-x3 += dx3;
-x4 += dx4;
+requestAnimationFrame(animate1);
 }
 
     animate1();
